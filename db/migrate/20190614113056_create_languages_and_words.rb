54e9f1be4f7b31b44cbf154f566b067070203987
WATANAPEI <info@wpei.dev>
2019-06-14 16:02:55 +0200

    add refs from words to languages

---
+++ db/migrate/20190614113056_create_languages_and_words.rb
@@ -0,0 +1,24 @@
+class CreateWords < ActiveRecord::Migration[5.2]
+  def change
+    create_table :languages do |t|
+      t.string :language_name, :null => false
+      t.string :created_by
+      t.string :last_updated_by
+      t.timestamps
+    end
+    create_table :words do |t|
+
+      t.string :word, :null => false
+      t.string :meaning, :null => false
+      t.references :word_lang
+      t.references :meaning_lang
+      t.string :created_by
+      t.string :last_updated_by
+      t.timestamps
+    end
+    add_foreign_key :words, :languages, column: :word_lang_id
+    add_foreign_key :words, :languages, column: :meaning_lang_id
+    
+  end
+end
+
