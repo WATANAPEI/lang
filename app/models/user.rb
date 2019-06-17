7855c610cc15bddf4bfcc81e12f5891c9cbafcc2
WATANAPEI <info@wpei.dev>
2019-06-17 21:10:09 +0200

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true
   include DeviseTokenAuth::Concerns::User
 end
