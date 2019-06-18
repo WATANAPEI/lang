811d0e0161c2fb09fedc1ee3ed75b8d9cf6ef914
WATANAPEI <info@wpei.dev>
2019-06-18 17:51:56 +0200

    finish model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name, email, password" do
+    user = User.new(
+      name: "Taro yamada",
+      email: "taroyamada@example.com",
+      password: "123456789"
+    )
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a name" do
+    user = User.new(
+      name: nil
+    )
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without an email" do
+    user = User.new(
+      email: nil
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "does not have a duplicate email" do
+    user = User.create(
+      name: "Taro yamada",
+      email: "taroyamada@example.com",
+      password: "123456789"
+    )
+    dup_user = User.new(
+      name: "Jiro Tanaka",
+      email: "taroyamada@example.com",
+      password: "jiroyamada1111"
+    )
+    dup_user.valid?
+    expect(dup_user.errors[:email]).to include("has already been taken")
+
+  end
 end
