b6c6480b1bccdc64722d475ca4b8ed5925ba9596
WATANAPEI <info@wpei.dev>
2019-06-20 17:52:57 +0200

    add controller spec(index,show,create,update)

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -12,30 +12,59 @@ RSpec.describe WordsController, type: :controller do
       expect(response).to have_http_status "200"
     end
   end
+
   describe "#show" do
     before do
       @word = FactoryBot.create(:word)
     end
 
-    it "responds successfully" do
-      get :show, params: {id: @word.id}
-      expect(response).to be_successful
+    context "with proper params" do
+      it "responds successfully" do
+        get :show, params: {id: @word.id}
+        expect(response).to be_successful
+      end
+
+      it "responds a 200 reponse" do
+        get :show, params: {id: @word.id}
+        expect(response).to have_http_status "200"
+      end
     end
 
-    it "responds a 200 reponse" do
-      get :show, params: {id: @word.id}
-      expect(response).to have_http_status "200"
+    context "with invalid id" do
+      it "responds 404 response"
     end
   end
 
   describe "#create" do
-    it "adds a word" do
-      word = FactoryBot.build(:word)
-      word_params = word.attributes.symbolize_keys.slice(:word, :meaning, :word_lang_id, :meaning_lang_id)
-      expect {
-        post :create, params: {word: word_params}
-      }.to change(Word, :count).by(1)
+    context "with proper params" do
+      it "adds a word" do
+        word = FactoryBot.build(:word)
+        word_params = word.attributes.symbolize_keys.slice(:word, :meaning, :word_lang_id, :meaning_lang_id)
+        expect {
+          post :create, params: {word: word_params}
+        }.to change(Word, :count).by(1)
+      end
+    end
+    context "with invalid params" do
+      it "responds error message"
     end
   end
 
+  describe "#update" do
+    before do
+      @word = FactoryBot.create(:word)
+    end
+    context "with proper params" do
+      it "modifies a word" do
+        word = FactoryBot.build(:word)
+        word_params = word.attributes.symbolize_keys.slice(:word, :meaning, :word_lang_id, :meaning_lang_id)
+        word_params[:meaning] = "changed_for_test"
+        patch :update, params: {id: @word.id, word: word_params }
+        expect(@word.reload.meaning).to eq "changed_for_test"
+      end
+    end
+    context "with invalid params" do
+      it "responds error message"
+    end
+  end
 end
