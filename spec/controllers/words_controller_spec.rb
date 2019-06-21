e20fc9aedbf65536ec1a801b0f3e3c5faa15285e
WATANAPEI <info@wpei.dev>
2019-06-21 08:45:05 +0200

    add an irregular tests for WordController#update and #destroy

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -76,19 +76,33 @@ RSpec.describe WordsController, type: :controller do
 
   describe "#update" do
     before do
-      @word = FactoryBot.create(:word)
+      @saved_word = FactoryBot.create(:word, meaning: "original_meaning")
+      @word = FactoryBot.build(:word)
+      @word_params = @word.attributes.symbolize_keys.slice(:word, :meaning, :word_lang_id, :meaning_lang_id)
+      @word_params[:meaning] = "changed_meaning"
     end
     context "with proper params" do
       it "modifies a word" do
-        word = FactoryBot.build(:word)
-        word_params = word.attributes.symbolize_keys.slice(:word, :meaning, :word_lang_id, :meaning_lang_id)
-        word_params[:meaning] = "changed_for_test"
-        patch :update, params: {id: @word.id, word: word_params }
-        expect(@word.reload.meaning).to eq "changed_for_test"
+        patch :update, params: {id: @saved_word.id, word: @word_params }
+        expect(@saved_word.reload.meaning).to eq "changed_meaning"
+      end
+
+      it "responds a 200 status" do
+        patch :update, params: {id: @saved_word.id, word: @word_params }
+        expect(response).to have_http_status "200"
+
       end
     end
-    context "with invalid params" do
-      it "responds error message"
+    context "with invalid word id" do
+      it "responds 404 error status" do
+        patch :update, params: {id: -1 , word: @word_params }
+        expect(response).to have_http_status "404"
+      end
+
+      it "does not update a word" do
+        patch :update, params: {id: -1 , word: @word_params }
+        expect(@saved_word.meaning).to eq "original_meaning"
+      end
     end
   end
 
@@ -103,7 +117,19 @@ RSpec.describe WordsController, type: :controller do
     end
 
     context "with invalid param" do
-      it "returns error"
+      before do
+        @word = FactoryBot.create(:word)
+      end
+      it "returns a 404 status" do
+        delete :destroy, params: {id: -1}
+        expect(response).to have_http_status "404"
+      end
+
+      it "does not delete a word" do
+        expect { 
+        delete :destroy, params: {id: -1}
+        }.to change(Word, :count).by(0)
+      end
     end
   end
 end
