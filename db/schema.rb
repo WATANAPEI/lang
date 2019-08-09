84cc1565583a36df2bc8e1f2ae3aed391e444439
WATANAPEI <info@wpei.dev>
2019-08-09 16:57:58 +0200

    refactor shema to use single quotation

--- db/schema.rb
+++ db/schema.rb
@@ -12,52 +12,52 @@
 
 ActiveRecord::Schema.define(version: 2019_06_14_113056) do
 
-  create_table "languages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "language_name", null: false
-    t.string "created_by"
-    t.string "last_updated_by"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'languages', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'language_name', null: false
+    t.string 'created_by'
+    t.string 'last_updated_by'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "name"
-    t.string "email"
-    t.text "tokens"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "created_by"
-    t.string "last_updated_by"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'provider', default: 'email', null: false
+    t.string 'uid', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.boolean 'allow_password_change', default: false
+    t.datetime 'remember_created_at'
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.string 'current_sign_in_ip'
+    t.string 'last_sign_in_ip'
+    t.string 'name'
+    t.string 'email'
+    t.text 'tokens'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'created_by'
+    t.string 'last_updated_by'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index ['uid', 'provider'], name: 'index_users_on_uid_and_provider', unique: true
   end
 
-  create_table "words", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "word", null: false
-    t.string "meaning", null: false
-    t.bigint "word_lang_id"
-    t.bigint "meaning_lang_id"
-    t.string "created_by"
-    t.string "last_updated_by"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["meaning_lang_id"], name: "index_words_on_meaning_lang_id"
-    t.index ["word_lang_id"], name: "index_words_on_word_lang_id"
+  create_table 'words', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'word', null: false
+    t.string 'meaning', null: false
+    t.bigint 'word_lang_id'
+    t.bigint 'meaning_lang_id'
+    t.string 'created_by'
+    t.string 'last_updated_by'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['meaning_lang_id'], name: 'index_words_on_meaning_lang_id'
+    t.index ['word_lang_id'], name: 'index_words_on_word_lang_id'
   end
 
-  add_foreign_key "words", "languages", column: "meaning_lang_id"
-  add_foreign_key "words", "languages", column: "word_lang_id"
+  add_foreign_key 'words', 'languages', column: 'meaning_lang_id'
+  add_foreign_key 'words', 'languages', column: 'word_lang_id'
 end
